346d6e597fcd438d17af9ed3b502dad943a5fb2f
GP <ganesh.prasannah@oslash.com>
2021-11-11 15:17:45 +0530

    chore: upgrade to Deno v1.16

--- dev_deps.ts
+++ dev_deps.ts
@@ -3,4 +3,4 @@ export type { Stub } from "https://deno.land/x/mock@0.10.1/mod.ts";
 export {
   assertEquals,
   assertThrowsAsync,
-} from "https://deno.land/std@0.112.0/testing/asserts.ts";
+} from "https://deno.land/std@0.114.0/testing/asserts.ts";
